8c5d6fd914493c9ddb663a729e78e5e01ab70169
Ekkeskit <126162842+Ekkeskit@users.noreply.github.com>
2024-02-21 13:38:37 +0100

    Test

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+@app.get("/items")
+async def root():
+    return {"greeting": "Help", "message": "Omg"}
